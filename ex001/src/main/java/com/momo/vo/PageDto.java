b6bfa7be467957076be9c6b044e177c1a4c30f3a
JinKyu <JinKyu@192.168.219.100>
2023-08-03 01:09:30 +0900

    ㅎㅎ

--- ex001/src/main/java/com/momo/vo/PageDto.java
+++ ex001/src/main/java/com/momo/vo/PageDto.java
@@ -25,7 +25,7 @@ public class PageDto {
 		//페이지 블록의 시작번호
 		this.startNo = this.endNo - 4;
 		
-		//총 게시물의 수를 페이지당 보여지는 게시물의 수로 나눠서 실제 끝페이지 번호를 구함
+		//총 게시물의 수를 페이지당 보여지는 게시물의 수로 나눠서 실제 끝 페이지 번호를 구함
 		realEnd = (int)Math.ceil((total*1.0)/cri.getAmount());
 		
 		//게시글이 없을때는 realEnd값이 0이되면서 페이지 끝을 누르면  예외발생
